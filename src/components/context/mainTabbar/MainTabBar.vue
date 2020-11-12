f43c4b8b1108b2bdc79c6a5546cf386a65b2650e
immor <1125584876@qq.com>
2020-11-12 14:28:56 +0800

    初始化框架

---
+++ src/components/context/mainTabbar/MainTabBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="main-tab-bar">
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <template v-slot:item-icon>
+          <img src="~@/assets/images/tabbar/home.svg"/>
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="~@/assets/images/tabbar/home-active.svg"/>
+        </template>
+        <template v-slot:item-text>
+          <div>首页</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/cateGory">
+        <template v-slot:item-icon>
+          <img src="~@/assets/images/tabbar/category.svg"/>
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="~@/assets/images/tabbar/category-active.svg"/>
+        </template>
+        <template v-slot:item-text>
+          <div>分类</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/car">
+        <template v-slot:item-icon>
+          <img src="~@/assets/images/tabbar/car.svg"/>
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="~@/assets/images/tabbar/car-active.svg"/>
+        </template>
+        <template v-slot:item-text>
+          <div>购物车</div>
+        </template>
+      </tab-bar-item>
+      <tab-bar-item path="/profile">
+        <template v-slot:item-icon>
+          <img src="~@/assets/images/tabbar/profile.svg"/>
+        </template>
+        <template v-slot:item-icon-active>
+          <img src="~@/assets/images/tabbar/profile-active.svg"/>
+        </template>
+        <template v-slot:item-text>
+          <div>我的</div>
+        </template>
+      </tab-bar-item>
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+  import TabBar from "@/components/common/tabbar/TabBar";
+  import TabBarItem from "@/components/common/tabbar/TabBarItem";
+
+  export default {
+    name: "MainTabBar",
+    components: {TabBar, TabBarItem},
+  }
+</script>
+
+<style scoped>
+  img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    margin-bottom: 2px;
+    vertical-align: middle;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,35 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+const Home = () => import("@/views/home/Home");
+const Category = () => import("@/views/category/Category");
+const Car = () => import("@/views/car/Car");
+const Profile = () => import("@/views/profile/Profile");
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: '首页',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/category',
+    name: '分类',
+    component: Category
+  },
+  {
+    path: '/car',
+    name: '购物车',
+    component: Car
+  },
+  {
+    path: '/profile',
+    name: '我的',
+    component: Profile
+  },
 ]
 
 const router = createRouter({
